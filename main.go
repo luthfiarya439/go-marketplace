ca6e0e589b4b1772b7f8930dcfcb0fa1cd64931c
Luthfi Aryaputra <luthfiaryaputra@gmail.com>
2024-06-26 22:12:50 +0700

    add admin middleware & test middleware

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go-marketplace/config"
 	"go-marketplace/controllers"
+	"go-marketplace/middleware"
 	"net/http"
 	"os"
 
@@ -24,6 +25,14 @@ func main() {
 	authRoute := router.Group("/auth")
 	authRoute.POST("login", controllers.Authenticate)
 	authRoute.POST("register", controllers.Register)
+
+	adminRoute := router.Group("/api/admin")
+	adminRoute.Use(middleware.AdminMiddleware)
+	{
+		router.GET("ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"message": "pong"})
+		})
+	}
 
 	port := os.Getenv("PORT")
 	router.Run(":" + port)

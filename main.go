eaee5cb774693ccc4ffb2728f0a2a941c6949c63
Luthfi Aryaputra <luthfiaryaputra@gmail.com>
2024-06-26 21:14:11 +0700

    add ping & gitignore

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "go-marketplace/config"
+import (
+	"go-marketplace/config"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func init() {
 	config.LoadEnv()
@@ -8,5 +14,11 @@ func init() {
 }
 
 func main() {
-	// router := gin.Default()
+	router := gin.Default()
+
+	router.GET("ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+	port := os.Getenv("PORT")
+	router.Run(":" + port)
 }

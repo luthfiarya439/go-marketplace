f875f0febfb0da3390bce74e5ca48712656dd157
Luthfi Aryaputra <luthfiaryaputra@gmail.com>
2024-06-26 22:05:12 +0700

    update user migration

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user')
 );
